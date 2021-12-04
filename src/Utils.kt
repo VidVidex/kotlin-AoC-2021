8ad17b9c98740a9ecebf873303e5e2d88f3a69f7
Vid Smole <vid.smole@gmail.com>
2021-12-04 19:10:42 +0100

    Add a function to read input to int array

--- src/Utils.kt
+++ src/Utils.kt
@@ -5,7 +5,19 @@ import java.security.MessageDigest
 /**
  * Reads lines from the given input txt file.
  */
-fun readInput(name: String) = File("src", "$name.txt").readLines()
+fun readInput(name: String) = File("src", name).readLines()
+
+fun readInputToIntArr(name: String): List<Int> {
+    val input = readInput(name)
+
+    val intInput = ArrayList<Int>()
+
+    for (value in input)
+        intInput.add(value.toInt())
+
+    return intInput
+}
+
 
 /**
  * Converts string to md5 hash.
